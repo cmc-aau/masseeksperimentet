10eab612f986b1bfcfec40d575c5c18fa1668988
Kasper Skytte Andersen <github@kasper.slmail.me>
2026-04-14 12:20:26 +0200

    add shiny app+gh action+readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Masseeksperimentet
+Code for community data analysis and shiny app.
+
+## Shiny app
+Export static app using [shinylive](https://posit-dev.github.io/r-shinylive/):
+```
+shinylive::export(appdir = "app", destdir = "docs")
+usethis::use_github_action(url="https://github.com/posit-dev/r-shinylive/blob/actions-v1/examples/deploy-app.yaml")
+httpuv::runStaticServer("./docs", headers = c(`Access-Control-Allow-Origin` = "*"))
+```
+
+Serving it through RStudio on BioCloud through OOD might not work, but it will locally. GitHub actions should build the site to serve it on pages.
---
+++ app/app.R
@@ -0,0 +1,39 @@
+library("leaflet")
+library("shiny")
+library("bslib")
+
+ui <- fluidPage(
+  titlePanel("Masseeksperimentet"),
+  leafletOutput("map", height = "90vh")
+)
+
+server <- function(input, output, session) {
+  MXmetadata <- read.csv("MXmetadata.csv")
+  
+  # Render the initial map
+  output$map <- renderLeaflet({
+    leaflet(MXmetadata) %>%
+      addTiles() %>%
+      addCircleMarkers(
+        lng = ~PointLongitude, lat = ~PointLatitude, 
+        layerId = ~LibID,
+        radius = 5, color = "blue"
+      )
+  })
+  
+  observeEvent(input$map_marker_click, {
+    click <- input$map_marker_click
+    LibID <- click$id
+    
+    # Show a modal with the pregenerated heatmap PNG
+    showModal(modalDialog(
+      title = paste("Sample:", LibID),
+      # Shinylive looks in 'www' for 'src'
+      img(src = file.path(paste0(LibID, ".png")), width = "100%"),
+      easyClose = TRUE,
+      footer = modalButton("Close")
+    ))
+  })
+}
+
+shinyApp(ui, server)
