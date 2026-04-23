eb078bee0ab2f78876aec8610f10b6252c48ff25
Kasper Skytte Andersen <github@kasper.slmail.me>
2026-04-23 15:23:25 +0200

    lints

--- app/app.R
+++ app/app.R
@@ -8,23 +8,23 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   MXmetadata <- read.csv("MXmetadata.csv")
-  
+
   output$map <- renderLeaflet({
     leaflet(MXmetadata) %>%
       addTiles() %>%
       addAwesomeMarkers(
-        lng = ~PointLongitude, lat = ~PointLatitude, 
+        lng = ~PointLongitude, lat = ~PointLatitude,
         layerId = ~LibID,
-        icon = awesomeIcons(library = 'ion', markerColor = 'blue')
+        icon = awesomeIcons(library = "ion", markerColor = "blue")
       )
   })
-  
+
   observeEvent(input$map_marker_click, {
     click <- input$map_marker_click
     selected_id <- click$id
-    
+
     sample_info <- MXmetadata[MXmetadata$LibID == selected_id, ]
-    
+
     showModal(modalDialog(
       title = paste("Prøve ID:", selected_id, "-", MXmetadata[MXmetadata$LibID == selected_id, "SampleID"]),
       img(src = paste0(selected_id, ".png"), width = "100%"),
